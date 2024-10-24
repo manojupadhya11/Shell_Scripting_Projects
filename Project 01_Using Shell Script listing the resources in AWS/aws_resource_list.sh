4d145849e8bcdada8f1248c724bb27bb12368e22
manojupadhya11 <manojupadhya11@gmail.com>
2024-10-24 09:53:02 +0530

    Project 1 Aws_resource_list.sh initial version

---
+++ Project 01_Using Shell Script listing the resources in AWS/aws_resource_list.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+######################################################################
+# This script will list all the resources in the AWS account
+# Author: Manoj Upadhya
+# Version: v0.0.1
+# 
+# Following are the supported AWS Services by the script
+# 1. EC2
+# 2. S3
+# 3. RDS
+# 4. DynamoDB
+# 5. Lambda
+# 6 .EBS
+# 7. ELB
+# 8. CloudFront 
+# 9. CloudWatch
+# 10. SNS
+# 11. SQS
+# 12. Route53
+# 13. VPC
+# 14. CloudFormation
+# 15. IAM
+# Usage: . /aws_resourc_list.sh <region> <service_name>
+# Example: ./aws_resource_list.sh us-east-1 EC2
+########################################################################
